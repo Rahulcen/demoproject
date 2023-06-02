9c371800896f0ed3fee1924dca488642d698cf5d
Rahulcen <rahulcen.er@gmail.com>
2023-06-02 16:54:50 +0530

    first commit

---
+++ src/test/java/com/example/awslamda1/DemoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awslamda1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
